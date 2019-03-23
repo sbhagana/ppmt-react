8969af36985ba5a737484399aef94600ceb9f0cc
Sumit Kumar <sumit.kumar@polestarllp.com>
2019-03-23 13:28:06 +0530

    all crud operations handled

--- ppmt/src/main/java/com/ppmtoolfullstack/ppmt/services/ProjectService.java
+++ ppmt/src/main/java/com/ppmtoolfullstack/ppmt/services/ProjectService.java
@@ -14,17 +14,54 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProjectService {
 
-    @Autowired
-    private ProjectRepository projectRepository;
+	@Autowired
+	private ProjectRepository projectRepository;
 
 	public Project saveOrUpdateProject(@Valid Project project) {
 		try {
 			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 			return projectRepository.save(project);
 		} catch (Exception e) {
-			throw new ProjectIdException("Project ID: " + project.getProjectIdentifier().toUpperCase() + " already exists");
+			throw new ProjectIdException(
+					"Project ID: " + project.getProjectIdentifier().toUpperCase() + " already exists");
 		}
 	}
 
-  
+	public Project findProjectByProjectIdentifier(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if (project == null)
+			throw new ProjectIdException("Project ID '" + projectId.toUpperCase() + "' doesn't exists");
+
+		return project;
+	}
+	
+	public Iterable<Project> findAllProjects() {
+		return projectRepository.findAll();
+	}
+	
+	public void deleteByProjectId(String projectId) {
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		
+		if(project == null)
+			throw new ProjectIdException("Can't delete the project. Project Id '"  + projectId.toUpperCase() + "' doesn't exists");
+		projectRepository.delete(project);
+	}
+	
+	public Project updateProject(Project projectToUpdate) {
+		
+		Project project =  projectRepository.findByProjectIdentifier(projectToUpdate.getProjectIdentifier().toUpperCase());
+		
+		if(project == null)
+			throw new ProjectIdException("Can't update the project. Project Id '"  + projectToUpdate.getProjectIdentifier().toUpperCase() + "' doesn't exists");
+		
+		
+		project.setDescription(projectToUpdate.getDescription());
+		project.setEnd_date(projectToUpdate.getEnd_date());
+		project.setProjectName(projectToUpdate.getProjectName());
+		project.setStart_date(projectToUpdate.getStart_date());
+		
+		projectRepository.save(project);
+		return project;
+	}
+
 }
--- ppmt/src/main/java/com/ppmtoolfullstack/ppmt/web/ProjectController.java
+++ ppmt/src/main/java/com/ppmtoolfullstack/ppmt/web/ProjectController.java
@@ -14,33 +14,59 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
+	@Autowired
+	ProjectService projectService;
 
-    @Autowired
-    ProjectService projectService;
+	@Autowired
+	ErrorValidationHandlerService errorValidationHandlerService;
 
-    @Autowired
-    ErrorValidationHandlerService errorValidationHandlerService;
-    
-    @PostMapping("")
-    private ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
-    	
-    	ResponseEntity<?> errorMap = errorValidationHandlerService.ErrorValidationHandler(result);
-    	
-    	if(errorMap != null) return errorMap;
-    	
-    	projectService.saveOrUpdateProject(project);
-    	return new ResponseEntity<Project>(project,HttpStatus.CREATED);
-    	
-    }
+	@PostMapping("")
+	private ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+		ResponseEntity<?> errorMap = errorValidationHandlerService.ErrorValidationHandler(result);
+
+		if (errorMap != null)
+			return errorMap;
+
+		projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+
+	}
+
+	@GetMapping("{projectId}")
+	public Project findProjectByProjectId(@PathVariable String projectId) {
+		return projectService.findProjectByProjectIdentifier(projectId);
+	}
+
+	@GetMapping("/all")
+	public Iterable<Project> findAllProjects() {
+		return projectService.findAllProjects();
+	}
+	
+	@DeleteMapping("{projectId}")
+	public ResponseEntity<?> deleteByProjectId(@PathVariable String projectId) {
+		projectService.deleteByProjectId(projectId);
+		return new ResponseEntity<String>("Project deleted successfully",HttpStatus.OK);
+	}
+	
+	@PutMapping
+	public ResponseEntity<?> updateExistingProject(@RequestBody Project project) {
+		 projectService.updateProject(project);
+		
+		return new ResponseEntity<String>("Project Updated Successfully",HttpStatus.OK);
+	}
 }
